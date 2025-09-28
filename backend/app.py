3d0aec85539ef6f1c30b32ca24d6d245a9072a15
Good'n Paul <73657550+Goodn6138@users.noreply.github.com>
2025-09-28 14:30:20 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for frontend-backend communication
+#CORS(app)  # Enable CORS for frontend-backend communication
 CORS(app, resources={r"/api/*": {"origins": [ "https://psi-vibe-coder.onrender.com"]}}# # replace with your actual Render frontend URL ]}})
 
 def run_python_code(code: str):
