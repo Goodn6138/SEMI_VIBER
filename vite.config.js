5e409830112e67cc6079a82125e91a705192495f
Good'n Paul <73657550+Goodn6138@users.noreply.github.com>
2025-09-30 17:19:45 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   root: './frontend',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend')
+    }
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true
   },
   server: {
-    port: 3000
+    port: 3000,
+    fs: {
+      allow: ['..']
+    }
   }
 });
